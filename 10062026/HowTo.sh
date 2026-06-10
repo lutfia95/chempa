00ac270d3fd6404bcfdf308ad91d6b8dcff2c357
lutfia95 <yynk1@hotmail.com>
2026-06-10 12:14:27 +0200

    json

--- 10062026/HowTo.sh
+++ 10062026/HowTo.sh
@@ -1,6 +1,6 @@
 table S1, SMILES Naked 
 table S3 sheet column smiles
-table S3 sheet column smiles!
+table S4 sheet column smiles!
 
 conda run -n chempa-convert python 10062026/convert_to_smiles.py
 conda run -n chempa-convert python 10062026/convert_to_smiles.py --output-dir 10062026/my_outputs
